5153741fcd0d6db7e7b5f59195b345564da85058
Norbiros <89837724+Norbiros@users.noreply.github.com>
2021-12-17 08:53:11 +0100

    Update Main.java

--- src/main/java/dev/jensderuiter/websk/Main.java
+++ src/main/java/dev/jensderuiter/websk/Main.java
@@ -12,6 +12,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import java.io.IOException;
 
