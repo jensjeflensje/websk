7c4fb8f5b59f20c96fda28ad60e1179f7d90ff68
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-01-21 10:37:55 +0100

    🚀 Added proper way to create folder, and config
    Also I added plugin instance :)

--- src/main/java/dev/jensderuiter/websk/Main.java
+++ src/main/java/dev/jensderuiter/websk/Main.java
@@ -9,6 +9,7 @@ import dev.jensderuiter.websk.utils.adapter.SkriptV2_3;
 import dev.jensderuiter.websk.utils.adapter.SkriptV2_6;
 import dev.jensderuiter.websk.web.Webserver;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,10 +22,12 @@ public final class Main extends JavaPlugin {
     private static SkriptAdapter skriptAdapter;
     public static Webserver webserver = null;
     public static boolean use26;
+    private static Main instance;
 
     @Override
     public void onEnable() {
         addon = Skript.registerAddon(this);
+        instance = this;
         try {
             addon.loadClasses("dev.jensderuiter.websk", "skript");
         } catch (IOException e) {
@@ -40,15 +43,16 @@ public final class Main extends JavaPlugin {
                 getLogger().warning("Update it now: https://github.com/jensjeflensje/websk/releases/latest" + updater.getLatest());
                 break;
             case EQUAL:
-                getLogger().fine("You are on the latest WebSK version! Well done!");
+                getLogger().info("You are on the latest WebSK version! Well done!");
                 break;
             case GREATER:
                 getLogger().warning("Detected a test version of WebSK. Please report bugs on our GitHub.");
                 break;
         }
         
-        //Simple auto creating files for plugin!
-        //Replace with `bukkit.pluginDirectory` in new updates to add config.yml!
+        // This creates config.yml and all other websk folders
+        this.saveDefaultConfig();
+
         try {
             Path path = Paths.get("plugins/WebSK/files/");
             Files.createDirectories(path);
@@ -61,7 +65,11 @@ public final class Main extends JavaPlugin {
         skriptAdapter = use26 ? new SkriptV2_6() : new SkriptV2_3();
         Main.use26 = use26;
     }
-
+    
+    public static Main getInstance() {
+        return instance;
+    }
+    
     public static SkriptAdapter getSkriptAdapter() {
         return skriptAdapter;
     }
